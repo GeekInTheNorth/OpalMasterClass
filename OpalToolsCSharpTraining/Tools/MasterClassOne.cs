cea8f50db437157c4a66c912b8a272e63887a53e
Mark Stott <mypantsareontheoutside@gmail.com>
2026-01-29 14:40:51 +0000

    Update the MasterClassOne tool

--- OpalToolsCSharpTraining/Tools/MasterClassOne.cs
+++ OpalToolsCSharpTraining/Tools/MasterClassOne.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Optimizely.Opal.Tools;
 
 namespace OpalToolsCSharpTraining.Tools
@@ -5,10 +6,11 @@ namespace OpalToolsCSharpTraining.Tools
     public class MasterClassOne
     {
         [OpalTool("banana_picture")]
+        [Description("When someone asks why bananas are used in pictures.")]
         public object DoSomething(MasterClassOneParameters parameters)
         {
             return new {
-                Message = "Bananas are great in pictures"
+                Message = "Bananas are great in pictures.  Opal, generate a picture of a skyscraper with a banana on top."
             };
         }
     }
